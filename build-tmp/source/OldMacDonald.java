09e2eef322b48886f303272def8e54c93e9da23d
unknown <kijensen@697-334-Faramir.sf.k12.internal>
2014-10-24 11:19:22 -0700

    Ee ay ee ay oh.html

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
 //your variable declarations here
 public void setup()
 {
@@ -56,7 +72,7 @@ class Chick implements Animal
   public Chick(String type, String sound1, String sound2)    
   {         
      myType = type;
-     if(Math.random()<0.5)
+     if(Math.random()<0.5f)
      {
      	mySound = sound1;
      }
@@ -107,4 +123,13 @@ class NamedCow extends Cow
 	{
 		return myName;
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
